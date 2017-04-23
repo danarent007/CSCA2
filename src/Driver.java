2da18f051f09c4c921d05753504f9b3fd9acbcab
danarent007 <danielvorster2009@gmail.com>
2017-04-23 08:51:04 +0200

    Added time output and counter for insertions + time taken for 1.

---
+++ src/Driver.java
@@ -0,0 +1,47 @@
+//Daniel Vorster
+//VRSDAN004
+//09 April 2017
+//Driver class
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+
+/**
+ * Driver class provides a simple interface for running the 3 programs.
+ * @author DanielVorster
+ * Driver class provides a simple interface for running the 3 programs.
+ */
+public class Driver
+{
+    
+    /**
+     * Driver class provides a simple interface for running the 3 programs.
+     * @param args
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException
+    {
+        Scanner s = new Scanner(System.in); 
+        System.out.println("Enter option followed by space, then param:\n(1): SearchAVL\n(2): SearchIt\n(3): InsertAVL\n(4): InsertIt");
+        String[] ans = s.nextLine().split(" ");
+        
+        switch(ans[0]){
+            case "1": 
+                SearchAVL.main(new String[]{ans[1]});
+                break;
+            case "2":
+                SearchIt.main(new String[]{ans[1]});
+                break;
+            case "3":
+                InsertAVL.main(new String[]{ans[1]});
+                break;
+            case "4":
+                InsertIt.main(new String[]{ans[1]});
+                       
+            
+            
+        }
+    }
+}
